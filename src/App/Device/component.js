6de39efebe15f001812bd8b5e8a0a9f96d308387
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-07-22 16:52:48 -0400

    Basic styles for app section (#6)

--- src/App/Device/component.js
+++ src/App/Device/component.js
@@ -1,73 +1,105 @@
 import React from "react";
 import { componentFromProp } from "recompose";
+import { Segment, List } from "semantic-ui-react";
+import styled from "styled-components";
+import Toggle from "react-toggle";
 
 const RenderComponent = componentFromProp("component");
 
-export default function Device({
+function Device({
+  className,
   controls = [],
-  handleToggle,
   handleSelect,
-  handleSlider
+  handleSlider,
+  handleToggle
 }) {
   return (
-    <div>
-      {controls.map((c, i) => {
-        switch (c.type) {
-          case "button": {
-            return (
-              <div key={`${c.name}-${i}`}>
-                {c.name}
-                <RenderComponent
-                  checked={c.currentValue === "on" ? true : false}
-                  component="input"
-                  name={c.alias}
-                  onClick={handleToggle}
-                  type="radio"
-                />
-              </div>
-            );
+    <div className={className}>
+      <Segment inverted textAlign="center">
+        CONTROLS
+      </Segment>
+      <List divided relaxed size="huge">
+        {controls.map((c, i) => {
+          switch (c.type) {
+            case "button": {
+              return (
+                <List.Item key={`${c.name}-${i}`}>
+                  <div className="control-container">
+                    <div>
+                      {c.name.toUpperCase()}
+                    </div>
+                    <RenderComponent
+                      checked={c.currentValue === "on" ? true : false}
+                      component={Toggle}
+                      icons={false}
+                      name={c.alias}
+                      onChange={handleToggle}
+                    />
+                  </div>
+                </List.Item>
+              );
+            }
+            case "slider": {
+              return (
+                <List.Item key={`${c.name}-${i}`}>
+                  <div className="control-container">
+                    <div>
+                      {c.name.toUpperCase()}
+                    </div>
+                    <RenderComponent
+                      component="input"
+                      key={`${c.name}-${i}`}
+                      name={c.alias}
+                      onInput={handleSlider}
+                      type="range"
+                      value={c.currentValue}
+                    />
+                  </div>
+                </List.Item>
+              );
+            }
+            case "select": {
+              return (
+                <List.Item key={`${c.name}-${i}`}>
+                  <div className="control-container">
+                    <div>
+                      {c.name.toUpperCase()}
+                    </div>
+                    <RenderComponent
+                      component="select"
+                      name={c.alias}
+                      onChange={handleSelect}
+                      value={c.currentValue}
+                    >
+                      <option value=""> -- Choose from Options -- </option>
+                      {c &&
+                        c.options &&
+                        c.options.map((o, i) => {
+                          return (
+                            <option key={`${o}-${i}`} value={o}>
+                              {o}
+                            </option>
+                          );
+                        })}
+                    </RenderComponent>
+                  </div>
+                </List.Item>
+              );
+            }
           }
-          case "slider": {
-            return (
-              <div key={`${c.name}-${i}`}>
-                {c.name}
-                <RenderComponent
-                  component="input"
-                  key={`${c.name}-${i}`}
-                  name={c.alias}
-                  onInput={handleSlider}
-                  type="range"
-                  value={c.currentValue}
-                />
-              </div>
-            );
-          }
-          case "select": {
-            return (
-              <div key={`${c.name}-${i}`}>
-                {c.name}
-                <RenderComponent
-                  component="select"
-                  name={c.alias}
-                  onChange={handleSelect}
-                  value={c.currentValue}
-                >
-                  <option value=""> -- Choose from Options -- </option>
-                  {c &&
-                    c.options &&
-                    c.options.map((o, i) => {
-                      return (
-                        <option key={`${o}-${i}`} value={o}>
-                          {o}
-                        </option>
-                      );
-                    })}
-                </RenderComponent>
-              </div>
-            );
-          }
-        }
-      })}
+        })}
+      </List>
     </div>
   );
 }
+
+export default styled(Device)`
+  .ui.segment {
+    border-radius: 0;
+  }
+  .control-container {
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
--- src/App/Home/component.js
+++ src/App/Home/component.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Segment, List } from "semantic-ui-react";
+import styled from "styled-components";
 
 import { replaceParams } from "Helpers";
 import Urls from "Urls";
 
-export default function Home({ devices }) {
+function Home({ devices, className }) {
   return (
-    <div>
-      {devices.map((d, i) => {
-        return (
-          <div key={`${d.name}-${i}`}>
-            <Link to={replaceParams(Urls.App.Device, { alias: d.alias })}>
-              {d.name}
-            </Link>
-          </div>
-        );
-      })}
+    <div className={className}>
+      <Segment inverted textAlign="center">
+        HOME
+      </Segment>
+      <List divided relaxed size="huge">
+        {devices.map((d, i) => {
+          return (
+            <List.Item key={`${d.name}-${i}`}>
+              <List.Content>
+                <Link to={replaceParams(Urls.App.Device, { alias: d.alias })}>
+                  <div className="device-name">
+                    {d.name}
+                  </div>
+                </Link>
+              </List.Content>
+            </List.Item>
+          );
+        })}
+      </List>
     </div>
   );
 }
+
+export default styled(Home)`
+  .ui.segment {
+    border-radius: 0;
+  }
+  .device-name {
+    margin-left: 20px !important;
+  }
+`;
